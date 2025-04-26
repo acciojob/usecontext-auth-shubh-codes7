13ad5e7a084c69457a850a696da7d9d212a0bb17
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-26 23:32:15 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import Auth from './Auth.js'
+import {createContext} from 'react'
+
+export const AuthContext = createContext()
 
 const App = () => {
+  const [flag, setFlag] = React.useState(false)
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <AuthContext.Provider value={flag}>
+      <h2>Click on the checkbox to get authenticated</h2>
+      <Auth />
+      <br></br>
+      <input type="checkbox" id="checkbox" onClick={()=>setFlag(prev => !prev)}/>
+      <label htmlFor="checkbox">I'm not a robot</label>
+    </AuthContext.Provider>
   )
 }
 
---
+++ src/components/Auth.js
@@ -0,0 +1,12 @@
+import {useContext} from 'react'
+import {AuthContext} from './App.js'
+import React from 'react'
+
+export default function Auth(){
+    const flag = useContext(AuthContext)
+    return(
+        <>
+            {flag ? "You are now authenticated, you can proceed" : "you are not authenticated"}
+        </>
+    )
+}

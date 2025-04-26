920470de5ad446327d9b64722a40e63e125ccbe7
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-26 23:35:05 +0530

    s

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -5,8 +5,8 @@ import React from 'react'
 export default function Auth(){
     const flag = useContext(AuthContext)
     return(
-        <>
+        <p className="authText">
             {flag ? "You are now authenticated, you can proceed" : "you are not authenticated"}
-        </>
+        </p>
     )
 }
